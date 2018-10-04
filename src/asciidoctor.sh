47bf3e996da752c8281de143b4153f5a7899ff28
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2018-10-04 09:22:10 +0200

    Use safe mode of asciidoctor

--- src/asciidoctor.sh
+++ src/asciidoctor.sh
@@ -6,23 +6,28 @@ DOCUMENT_ID='15-078'
 DOCUMENT_PATH=$DOCUMENT_ID
 DOCUMENT_SRC="OGC_${DOCUMENT_ID}.adoc"
 DOCUMENT_TOF="pre_0_tof.adoc"
-DOCUMENT_HTML="../../dist/${DOCUMENT_ID}/${DOCUMENT_ID}.html"
-DOCUMENT_PDF="../../dist/${DOCUMENT_ID}/${DOCUMENT_ID}.pdf"
+DOCUMENT_HTML="${DOCUMENT_ID}.html"
+DOCUMENT_PDF="${DOCUMENT_ID}.pdf"
+DIST_DIR="../../dist"
 
 echo $SCRIPTPATH/${DOCUMENT_PATH}
 cd $SCRIPTPATH/${DOCUMENT_PATH}
 echo "Working on ${DOCUMENT_ID}..."
 
+mkdir "${DIST_DIR}"
+
 echo "Genering Table of Figures..."
 ../generate_tof.rb $DOCUMENT_SRC > $DOCUMENT_TOF
 
 echo "Genering HTML..."
-asciidoctor -a allow-uri-read -o $DOCUMENT_HTML $DOCUMENT_SRC
+asciidoctor --safe -o $DOCUMENT_HTML $DOCUMENT_SRC
+cp "${DOCUMENT_HTML}" "${DIST_DIR}/${DOCUMENT_HTML}"
 
 echo "Genering PDF..."
-asciidoctor-pdf -a allow-uri-read -o $DOCUMENT_PDF $DOCUMENT_SRC
+asciidoctor-pdf --safe -o $DOCUMENT_PDF $DOCUMENT_SRC
+cp "${DOCUMENT_PDF}" "${DIST_DIR}/${DOCUMENT_PDF}"
 
 echo "copying images..."
-cp -Rv "images" "../../dist/${DOCUMENT_ID}/"
+cp -Rv "images" "${DIST_DIR}/images"
 
 echo "Done."
