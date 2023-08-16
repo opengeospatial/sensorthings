3f06314e2ddb9d575296f6e926256be8c8597987
Hylke van der Schaaf <hylke.vds@gmail.com>
2023-08-16 11:57:29 +0200

    Improved structure

--- buildInDocker.sh
+++ buildInDocker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-metanorma compile --agree-to-terms -t ogc -x xml,html,doc document.adoc
+metanorma compile --agree-to-terms -t ogc -x xml,html,doc 23-019.adoc
 chown -R 1000:1000 /metanorma
