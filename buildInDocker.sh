76f0a3709ecb3252fc313050a9906693aeb8d2f2
Hylke van der Schaaf <hylke.vds@gmail.com>
2025-02-11 12:06:11 +0100

    Updated build script

--- buildInDocker.sh
+++ buildInDocker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm 23-019.doc 23-019.html 23-019.err.html 23-019.xml 23-019.xml.abort 23-019.presentation.xml
-metanorma compile --agree-to-terms -t ogc -x xml,html,doc 23-019.adoc
+rm 23-019.doc 23-019.html 23-019.err.html 23-019.xml 23-019.xml.abort 23-019.presentation.xml plantuml/*.png
+metanorma compile --agree-to-terms -t ogc -x xml,html 23-019.adoc
 chown -R 1000:1000 /metanorma
